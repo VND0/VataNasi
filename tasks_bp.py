524d8fa84e529378247b5e9543be00887db6a89d
nikita <varlamov577123@gmail.com>
2024-07-05 22:22:16 +0300

    Убрано упоминание режима моментальной проверки.

--- tasks_bp.py
+++ tasks_bp.py
@@ -32,22 +32,20 @@ def preferences_page(mode: int):
     categories_names = db.get_categories_of_user(current_user.id)
     kwargs = {"categories": categories_names}
     if request.method == "POST":
-        instant_check = request.form.get("check_type") == "on"
         chosen_categories = [c for c in categories_names if request.form.get(c) == "on"]
         amount_raw = request.form.get("amount")
 
-        if instant_check:
-            kwargs["message"] = "Моментальная проверка в разработке"
-        elif not chosen_categories:
+        if not chosen_categories:
             kwargs["message"] = "Не выбраны категории"
         elif amount_raw and (amount_raw[0] in ("0", "-") or not amount_raw.isdigit()):
             kwargs["message"] = "Неверный формат введенного числа"
         else:
             amount = 0 if not amount_raw else int(amount_raw)
             return redirect(
-                f"/task/{mode}?ic={int(instant_check)}{''.join(f'&cat={c}' for c in chosen_categories)}&amount={amount}"
+                f"/task/{mode}?ic={0}{''.join(f'&cat={c}' for c in chosen_categories)}&amount={amount}"
             )
 
+    # TODO: поправить костыль с instant check (которой больше нет)
     return render_template("mode_preferences_page.html", is_authenticated=current_user.is_authenticated, **kwargs)
 
 
